2ca7119e6a87fb84b00c4e73db0f379e3d594aea
Matthias Koch <ithrowexceptions@gmail.com>
2023-08-10 13:54:50 +0200

    feat: add inlay hints

---
+++ src/CodeEditing/InlayHints.cs
@@ -0,0 +1,17 @@
+namespace CodeEditing;
+
+public class InlayHints
+{
+    public void M()
+    {
+        // TODO: add tuple name
+        var dict = new List<(string FirstName, string LastName, DateTime?, bool Flagged)>();
+
+        dict.Add(("Sasha", "Ivanova", DateTime.UnixEpoch, false));
+        dict.Add(("Maarten", "Balliauw", null, true));
+        dict.Add(("Rachel", "Appel", DateTime.Now, false));
+
+        if (dict.First() is ("Maarten", _, _, _))
+            Console.WriteLine("HELP!");
+    }
+}
