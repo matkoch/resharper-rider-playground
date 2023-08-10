a00e98233f1a31ebf624694057e62b75ef89ce23
Matthias Koch <ithrowexceptions@gmail.com>
2023-08-10 13:09:59 +0200

    feat: add var navigation

---
+++ src/Navigation/VarNavigation.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Immutable;
+
+namespace Navigation;
+
+public class VarNavigation
+{
+    public void M()
+    {
+        // Navigation: Ctrl-Click on var
+        // Navigation: Find Usages on var
+        var people = new[] { new bool?() }.ToImmutableArray();
+    }
+}
