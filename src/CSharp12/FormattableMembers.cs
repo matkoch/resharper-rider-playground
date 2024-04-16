993b7b0b25b5ab2b04fe5f2d3b050397c2950015
Matthias Koch <ithrowexceptions@gmail.com>
2024-04-16 12:42:47 +0200

    feat: formattable members

---
+++ src/CSharp12/FormattableMembers.cs
@@ -0,0 +1,8 @@
+namespace CSharp12;
+
+public class FormattableMembers
+{
+    record struct Point(int X, int Y) : ISpanFormattable, IUtf8SpanFormattable;
+
+    // https://twitter.com/neuecc/status/1772925680293904849
+}
