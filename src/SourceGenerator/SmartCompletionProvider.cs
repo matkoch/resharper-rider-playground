dec3749c270d594070a1649839822809fc602bab
Matthias Koch <ithrowexceptions@gmail.com>
2025-12-02 12:01:23 +0100

    feat: source generator

---
+++ src/SourceGenerator/SmartCompletionProvider.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.CodeAnalysis;
+
+namespace SourceGenerator;
+
+[Generator]
+[SuppressMessage("MicrosoftCodeAnalysisCompatibility", "RS1042:Implementations of this interface are not allowed")]
+public class SmartCompletionProvider : ISourceGenerator
+{
+    public void Initialize(GeneratorInitializationContext context)
+    {
+    }
+
+    public void Execute(GeneratorExecutionContext context)
+    {
+        var yamlFiles = context.AdditionalFiles
+            .Where(x => x.Path.EndsWith(".yaml", StringComparison.OrdinalIgnoreCase));
+        context.AddSource("YamlFiles.cs", $$"""
+            public static class YamlFiles
+            {
+            {{string.Join("\n", yamlFiles.Select(x =>
+                $"""public static string {Path.GetFileNameWithoutExtension(x.Path)} = "{x.Path}";"""))}}
+            }
+            """);
+    }
+}
